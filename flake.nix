be3083e17748b7a38fdc88767ad2deacd99e854f
Benedikt Spies <benedikt.spies@gmail.com>
2024-12-02 01:42:50 +0100

    add package

--- flake.nix
+++ flake.nix
@@ -11,7 +11,17 @@
             inherit system;
           };
         in 
-        { 
+        {
+          packages.default = pkgs.stdenv.mkDerivation {
+            name = "pop-shell";
+            src = self;
+            makeFlags = [
+              "XDG_DATA_HOME=$(out)/share"
+            ];
+            nativeBuildInputs = with pkgs; [
+              typescript
+            ];
+          };
           devShells.default = pkgs.mkShell {
             buildInputs = with pkgs; [
               typescript
